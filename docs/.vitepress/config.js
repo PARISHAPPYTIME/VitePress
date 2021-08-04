eb6eb89c9df3c33cc041fbd9b5e4dcbe59dc1c78
烟熏三文鱼 <1678803637@qq.com>
2021-08-04 18:10:11 +0800

    add

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,20 +1,17 @@
 module.exports = {
     title: 'Hello VitePress',
     description: 'Just playing around.',
-    markdown: {
-        lineNumbers: true
-    },
     base: '/VitePress/',
     // 主题配置
     themeConfig: {
         //   头部导航
         nav: [
             { text: '首页', link: '/' },
-            { text: 'Redux', link: '/redux-start' },
         ],
         //   侧边导航
         sidebar: [
-            { text: '我的', link: '/mine/' }
+            { text: 'Redux', link: '/redux-start' },
+            { text: 'Other Links', link: '/links' }
         ]
     }
 }
